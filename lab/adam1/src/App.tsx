ee7104efdf7f1966fd61bcfa2df79bfa5b7d718e
Sushant Agrawal <capitalch@gmail.com>
2026-01-20 11:14:50 +0530

    setup

---
+++ lab/adam1/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button";
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-green-600">
+        Tailwind v4 is working
+        <Button>Test</Button>
+      </h1>
+    </div>
+  );
+}
+
+export default App
--- notes/knowledgebase.md
+++ notes/knowledgebase.md
@@ -1,2 +1,5 @@
 ## Service management software
+# pnpm: node 17 has it
+	- Steps:
+		- corepack enable pnpm
 # pnpm dlx
